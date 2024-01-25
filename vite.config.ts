1167c357ef45555230bf9c87e0ee2935425c39e4
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-01-25 08:40:58 -0300

    feat: Initialize the React app using Vite

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': '/src',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@hooks': '/src/utils/hooks',
+      '@data': '/src/data',
+    },
+  },
+});
