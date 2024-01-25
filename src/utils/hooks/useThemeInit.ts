af8f002e98775b0ac3e44912387a5601ba4806d0
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-01-25 08:51:00 -0300

    feat: Add a callback hook to manage the theme

---
+++ src/utils/hooks/useThemeInit.ts
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+/**
+ * Initialize the theme of the web page.
+ */
+const useThemeInit = () => {
+  useEffect(() => {
+    const isDarkMode = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    document.documentElement.classList.toggle('dark', isDarkMode);
+    localStorage.theme = isDarkMode ? 'dark' : 'light';
+  }, []);
+};
+
+export default useThemeInit;
