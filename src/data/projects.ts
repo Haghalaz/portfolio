8eeffffa13e4ecb8d441f8d91fc6c9d8b44d41b6
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-02-15 10:36:28 -0300

    feat: Add a new project into showcase

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,4 +1,5 @@
 import PortfolioCover from '@assets/imgs/portfolioCover.png';
+import BounceCover from '@assets/imgs/bounceCover.png';
 
 export type ProjectsType = {
   id: number;
@@ -16,6 +17,14 @@ const PROJECTS: ProjectsType[] = [
     description: 'PortfolioDescription',
     src: PortfolioCover,
     repo: 'https://github.com/Haghalaz/portfolio',
+    skills: ['vite', 'react', 'ts', 'tailwind'],
+  },
+  {
+    id: 2,
+    title: 'Bounce',
+    description: 'BounceDescription',
+    src: BounceCover,
+    repo: 'https://github.com/Haghalaz/bounce',
     skills: ['vite', 'react', 'ts', 'tailwind'],
   },
 ];
