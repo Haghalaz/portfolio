9ce5fedb5558717ba971649405bdbdaa512d3381
Luiz Coelho <luiz.adalberto@selettra.com.br>
2024-05-09 14:34:04 -0300

    Undo changes

--- src/utils/hooks/useCallbackRef.ts
+++ src/utils/hooks/useCallbackRef.ts
@@ -4,7 +4,7 @@ import { useRef, useEffect, useMemo } from 'react';
  * A custom hook that converts a callback to a ref to avoid triggering re-renders when passed as a
  * prop or avoid re-executing effects when passed as a dependency
  */
-function useCallbackRef<T extends (...args: never[]) => never>(callback: T | undefined): T {
+function useCallbackRef<T extends (...args: any[]) => any>(callback: T | undefined): T {
   const callbackRef = useRef(callback);
 
   useEffect(() => {
